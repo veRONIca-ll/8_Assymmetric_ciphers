ac80df1f2d52ffcbe1297c696dd5b5e50b8f1a4a
root <vmolchanovag@mail.ru>
2019-12-24 08:22:37 +0300

    quite good

--- client.py
+++ client.py
@@ -1,8 +1,24 @@
 import socket
 import pickle
 
+def en_de_crypt(msg, key):
+	secret = [chr(ord(msg[i])^key) for i in range(len(msg))]
+	return ''.join(secret)
+
+def send_(conn, msg, p_k):
+	sock.send(pickle.dumps(en_de_crypt(msg, K)))
+
+def recv_(conn, k):
+	msg=en_de_crypt(pickle.loads(sock.recv(1024)), k)
+	return msg
+
 HOST = '127.0.0.1'
-PORT = 8080
+while True:										
+	PORT=int(input('Enter port: '))
+	if 1024<=PORT<=65525:
+		break
+	else:
+		print('You made a mistake. Try again!')
 
 sock = socket.socket()
 sock.connect((HOST, PORT))
@@ -10,5 +26,21 @@ sock.connect((HOST, PORT))
 p, g, a = 7, 5, 3
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+
+B = pickle.loads(sock.recv(1024))
+
+K = B ** a % p
+
+msg = input()
+while True: 
+	try:
+		if msg=='exit':
+			break
+		send_(sock, msg, K)
+		print('...<msg is sent>...')
+		print(recv_(sock, K))
+		msg=input()
+	except:
+		break
 
 sock.close()
--- server.py
+++ server.py
@@ -1,15 +1,49 @@
 import socket
 import pickle
+import random
+
+
+def en_de_crypt(msg, key):
+	secret = [chr(ord(msg[i])^key) for i in range(len(msg))]
+	return ''.join(secret)
+
+def send_(conn, msg, p_k):
+	conn.send(pickle.dumps(en_de_crypt(msg, p_k)))
+
+def recv_(conn, k):
+	msg=en_de_crypt(pickle.loads(conn.recv(1024)), K)
+	return msg
 
 HOST = '127.0.0.1'
-PORT = 8080
-
 sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
+
+port = 8080     		
+while port!=65525:
+	try:
+		sock.bind(('',port))
+		print('The port is {}'.format(port))
+		break
+	except:
+		print('The port {} is not available. Checking new one...')
+		port+=1
+
+sock.listen(0)			
+sock.setblocking(1)
 conn, addr = sock.accept()
 
+b = random.randint(1, 100)
+
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+p, g, A = pickle.loads(msg)
+
+B = g ** b %p
+conn.send(pickle.dumps(B))
+
+K = A ** b %p
+while True:
+	
+	msg = recv_(conn, K)
+	print(msg)
+	send_(conn, 'понял принял', K)
 
 conn.close()
